5f68399ff3cc8d5f617847c1457f6b46e11d22b2
Arya Krishna <dr.aryakris@outlook.com>
2021-11-29 00:09:45 -0800

    trying to do log Nums algorithm

--- Log Nums/log-num.js
+++ Log Nums/log-num.js
@@ -1,4 +1,18 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function (num) { };
+var logNums = function (num) {
+if (num<1){
+    console.error ('num must be greater than 1 ');
+    return;
+}
+    if (!Number.isInteger(num)){
+        // The Math.trunc() function returns the integer part of a number by removing any fractional digits.
+        num = Math.trunc(num);
+    }
+        for (var i = 1; i<= num; i++){
+            console.info(i);
+        }
+    
+}
+num(10);
